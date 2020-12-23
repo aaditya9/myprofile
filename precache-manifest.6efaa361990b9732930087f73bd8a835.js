735904a0890abb1f1a8f131766df325df2ddf649
Aditya Patil <adityapatil@Adityas-MacBook-Air.local>
2020-12-24 01:24:58 +0530

    Updates

---
+++ precache-manifest.6efaa361990b9732930087f73bd8a835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "599cec907f07b2571c4e",
+    "url": "/myprofile/static/css/main.d507f98d.chunk.css"
+  },
+  {
+    "revision": "599cec907f07b2571c4e",
+    "url": "/myprofile/static/js/main.599cec90.chunk.js"
+  },
+  {
+    "revision": "6ffbbab58a218cfc9588",
+    "url": "/myprofile/static/js/runtime~main.6ffbbab5.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/myprofile/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2445a801c6caa3602a958a1db442fcb6",
+    "url": "/myprofile/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.980682752786fbd80c62578681a52ac1.js"
+  "/myprofile/precache-manifest.6efaa361990b9732930087f73bd8a835.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me/index.html", {
+workbox.routing.registerNavigationRoute("/myprofile/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
