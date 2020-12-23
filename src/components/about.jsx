25fc5419191efc61cd733cd6261457412decf6a1
Aditya Patil <adityapatil@Adityas-MacBook-Air.local>
2020-12-24 01:53:59 +0530

    update about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I have completd masters in computer science.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I have completed masters in computer science.I love exploring new technologies and am often amazed by the progress we as a human species have made so far in the recent years (apart from the headphone-jack part!!)</p>
+                    <p>I have started reflecting my ideas and thoughts through the medium of words recently so spelling and grammar mistakes are very often.You can write me back if you spot any and don't want to live anymore :P </p>
                     </div>
                 </div>
                 </div>
